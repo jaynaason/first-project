21175053eaf0f9c2824bc99f22e634aad98986f9
jaynaason <42560159+jaynaason@users.noreply.github.com>
2018-08-20 21:17:15 -0500

    Add files via upload

---
+++ first-run.py
@@ -0,0 +1,27 @@
+# Emotional levels
+sad = 0
+happiness = 0
+anger = 0
+
+while True:
+    print '\n\nWelcome to this game'
+    intro = raw_input('Would you like to play? ')
+    if intro == "yes":
+        print 'Cool lets get started!'
+        break
+    elif intro == "no":
+        print 'Thats cool next time!'
+        break
+    else:
+        print 'Please try again'
+        continue
+
+print '\nThis game is too see your emotional state'
+lonely = raw_input('Do you feel lonely now? ')
+if lonely == 'yes':
+    sad += 1
+elif lonely == 'no':
+    lonely_2 = raw_input('are you mad')
+
+
+
